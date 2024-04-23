a84f941a32bf111f798a94539e8cb273ca648b73
Oksana Petrenko <oksanapetrenko@Oksanas-MacBook-Air.local>
2024-04-23 16:00:50 +0300

    First React branch

---
+++ blog/src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const jsxEl = <h1>I am a JSX element</h1>
+
+const Header = (
+  <header> 
+    <h1>Welcome to React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+  </header>
+)
+
+const Footer = (
+  <p>Copyright &copy; 2024</p>
+)
+
+function App() {
+  const [count, setCount] = useState(0)
+  const today = new Date()
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+      {jsxEl}
+
+      {Header}
+
+      <h1>Welcome to React</h1>
+      <h2 style={{color:'orange'}}>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+
+      {/* <h3 style={{backgroundColor:'blue', color:'gold'}}>It's today: {today.toLocaleDateString()} </h3> */}
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      {Footer}
+    </>
+  )
+}
+
+export default App
