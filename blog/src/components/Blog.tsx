5d0f93accf78acbbfd50b098582bfb7fcaf5506a
JaneDoessen <okssana.petrenko@gmail.com>
2024-04-30 17:29:24 +0300

    Exercise 25 is added

--- blog/src/components/Blog.tsx
+++ blog/src/components/Blog.tsx
@@ -3,14 +3,14 @@ const Blog = (props:any) => {
 
   return (
     <article className='post'>
-//             <div className='cover-container'>
-//               <img src={post.cover} alt={post.title} />
-//             </div>
-//             <div className='post-footer'>
-//               <h3>{post.title} {post.id}</h3>
-//               <p>{post.content}</p>
-//             </div>
-//         </article>
+           <div className='cover-container'>
+             <img src={post.cover} alt={post.title} />
+          </div>
+          <div className='post-footer'>
+              <h3>{post.title} {post.id}</h3>
+              <p>{post.content}</p>
+            </div>
+        </article>
   );
 }
 
